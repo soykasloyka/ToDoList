2cfa910112d9d9e58d62037fa750add4098472d4
Soyka <petr.emelianov@icloud.com>
2022-09-30 11:01:58 +0300

    Deleting the tasks

--- main.js
+++ main.js
@@ -5,18 +5,20 @@ const todosWrapper = document.querySelector('.todos-wrapper');
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
+let todoItemElems = [];
+
 function Task(description) {
   this.description = description;
   this.completed = false;
 }
-
+// template for tasks
 const createTemplate = (task, index) => {
   return `
     <div class="todo-item ${task.completed ? 'checked' : ''}">
       <div class="description">${task.description}</div>
       <div class="buttons">
-        <input class="btn-complete" type="checkbox" ${task.completed ? 'completed' : ''}>
-        <button class="btn-delete">DELETE</button>
+        <input onclick ="completeTask(${index})" class="btn-complete" type="checkbox" ${task.completed ? 'completed' : ''}>
+        <button onclick ="deleteTask(${index})" class="btn-delete">DELETE</button>
       </div>
     </div>
   `
@@ -27,7 +29,8 @@ const fillHtmlList = () => {
   if(tasks.length > 0) {
     tasks.forEach((item, index) => {
       todosWrapper.innerHTML += createTemplate(item, index);
-    })
+    });
+    todoItemElems = document.querySelectorAll('.todo-item');
   }
 }
 
@@ -36,6 +39,18 @@ fillHtmlList();
 const updateLocal = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+//tasks completing
+const completeTask = index => {
+  tasks[index].completed = !tasks[index].completed;
+  if(tasks[index].completed) {
+    todoItemElems[index].classList.add('checked');
+  } else {
+    todoItemElems[index].classList.remove('checked');
+  }
+  updateLocal();
+  fillHtmlList();
+}
+
 
 addTaskBtn.addEventListener('click', () => {
   tasks.push(new Task(deskTaskInput.value));
@@ -43,3 +58,12 @@ addTaskBtn.addEventListener('click', () => {
   fillHtmlList();
   deskTaskInput.value = '';
 })
+//tasks deleting
+const deleteTask = index => {
+todoItemElems[index].classList.add('delition');
+  setTimeout(() => {
+    tasks.splice(index, 1);
+    updateLocal();
+    fillHtmlList();
+  },700)
+}
